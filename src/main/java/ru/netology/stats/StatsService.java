925cc6033afc990b8f903908393b5486b7bab245
hYxA <zybu@live.ru>
2020-10-16 10:23:13 +0300

    create test

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -6,51 +6,66 @@ public class StatsService {
     int maxDealsIndex;
     int minDealsIndex;
     int summ;
+    int index;
     long average;
     int belowAverage = 0;
     int aboveAverage = 0;
 
-    public int sumCalculate(int deals, int summ) {
-        summ;
+    public int sumCalculate(int[] deals, int summ) {
         for (int deal : deals) {
-            summ += deal
+            summ += deal;
         }
-        return summ
+        return summ;
     }
 
     public long averageCalculate(int length, int summ, long average) {
         average = summ / length;
-        return average
+        return average;
     }
 
 
     // забыл, как вернуть номер "такого-то" элемента массива
     // поэтому немного некрасиво получилось
-    public int topSales(int deals) {
-        int index = 1;
+    public int topSalesMax(int[] deals, int index) {
+        index = 1;
         int maxDeals = deals[0];
-        int minDeals = deals[0];
         for (int deal : deals) {
-            if (minDeals < deal) {
-                minDeals = deal;
-                minDealsIndex = index;
-            }
             if (maxDeals > deal) {
                 maxDeals = deal;
                 maxDealsIndex = index;
             }
             index += 1;
         }
+        return maxDealsIndex;
     }
 
-    public int belowAboveAverage(long average, int deals, int belowAverage, int aboveAverage) {
+    public int topSalesMin(int[] deals, int index) {
+        index = 1;
+        int minDeals = deals[0];
+        for (int deal : deals) {
+            if (minDeals < deal) {
+                minDeals = deal;
+                minDealsIndex = index;
+            }
+            index += 1;
+        }
+        return minDealsIndex;
+    }
+
+    public int belowAverage(long average, int[] deals, int belowAverage) {
         for (int deal : deals) {
             if (deal < average) {
                 belowAverage += 1;
             }
+        }
+    return belowAverage;}
+
+    public int aboveAverage(long average, int[] deals, int aboveAverage) {
+        for (int deal : deals) {
+
             if (deal > average) {
                 aboveAverage += 1;
             }
         }
-    }
+        return aboveAverage;}
 }
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,8 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+
+}
