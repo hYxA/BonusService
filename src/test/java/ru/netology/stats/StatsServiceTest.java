2c230669ecd664d47d17fb918bfe6f67a52359c2
hYxA <zybu@live.ru>
2020-10-16 12:30:45 +0300

    min deals index

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -8,28 +8,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatsServiceTest {
     @ParameterizedTest
     @CsvSource(value ={
-            "'8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18', 180, 15, 8, 9, 5, 5"    //
-    });
+            "'8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18', 180, 15, 8, 9, 5, 5"
+    })
     StatsService SService = new StatsService();
 
 
     void calculateSumm(int[] deals, int expectedSumm, long expectedAverage, int expectedMaxDealsIndex,
                        int expectedMinDealsIndex, int expectedBelowAverage, int expectedAboveAverage) {
 
-        int summ = SService.sumCalculate(int deals[], expectedSumm);
-
+        int summ = SService.sumCalculate(int[] deals, expectedSumm);
         assertEquals(expectedSumm, summ);
 
     }
 
     void calculateAverage(int[] deals, int expectedSumm, long expectedAverage, int expectedMaxDealsIndex,
                           int expectedMinDealsIndex, int expectedBelowAverage, int expectedAboveAverage) {
-        long  = SService.averageCalculate(int deals[], expectedAverage);
+
+        long average = SService.averageCalculate(int[] deals, expectedAverage);
+        assertEquals(expectedAverage, average);
+
     }
 
+    void calculateMaxDealsIndex(int[] deals, int expectedSumm, long expectedAverage, int expectedMaxDealsIndex,
+                          int expectedMinDealsIndex, int expectedBelowAverage, int expectedAboveAverage) {
 
+        int maxDealsIndex = SService.maxDealsIndex( int[] deals, expectedMaxDealsIndex);
+        assertEquals(expectedMaxDealsIndex, maxDealsIndex);
 
+    }
 
+    void calculateMinDealsIndex(int[] deals, int expectedSumm, long expectedAverage, int expectedMaxDealsIndex,
+                                int expectedMinDealsIndex, int expectedBelowAverage, int expectedAboveAverage) {
 
+        int minDealsIndex = SService.minDealsIndex( int[] deals, expectedMinDealsIndex);
+        assertEquals(expectedMinDealsIndex, minDealsIndex);
+
+    }
 
 }
