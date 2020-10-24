26a10e9793dd65cbcf63488758321174fa6967c7
hYxA <zybu@live.ru>
2020-10-24 12:35:58 +0300

    remove old imports

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.stats;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
